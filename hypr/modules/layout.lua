dc78b4c2c6bfcc78f359b473dafa64feb6092a53
Oleksandr Tarabanko <olexandrtarabanko@gmail.com>
2026-08-04 12:43:14 +0200

    snap rule

--- hypr/modules/layout.lua
+++ hypr/modules/layout.lua
@@ -1,7 +1,11 @@
 -- See https://wiki.hypr.land/Configuring/Layouts/Dwindle-Layout/ for more
 hl.config({
     general = {
-        layout = "scrolling"
+        layout = "scrolling",
+        snap = {
+            enabled = true,
+            monitor_gap = 0, -- prevent windows from going past monitor edges
+        },
     },
     dwindle = {
         preserve_split = true, -- You probably want this
